ece3d7cad0f4ab67c78268c0c5f384ded4548b0f
D1n0sXIX <alejmaman@gmail.com>
2025-09-16 18:12:22 +0200

    19-09-2025
    Primero ejercicios hechos en clase de repaso de cursos anteriores

---
+++ Tema1/JavaScript/HolaMundo.js
@@ -0,0 +1,12 @@
+//var es variable global
+var miNumero= 8;
+
+// let es solo existe dentro del {}
+let nombreDeTuMama = "Ana"
+// const Como let pero sirve para variables que no queremos cambiar
+const typoPersona = "Mama"
+
+// Creaccion de objeto
+let myVar = {Name: "Jonh", Age: 46};
+console.log(miNumero);
+console.log(myVar);
